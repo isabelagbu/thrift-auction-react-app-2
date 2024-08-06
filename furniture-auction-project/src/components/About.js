d4208f4f488495e1a709af948b38c3b45452a6d1
Isabel Agbu <isabelxcoding@gmail.com>
2024-08-05 21:26:28 -0700

    updated about

--- furniture-auction-project/src/components/About.js
+++ furniture-auction-project/src/components/About.js
@@ -4,7 +4,8 @@ const About = (props) => (
   <div className="main-content">
     <h1>{props.title}</h1>
     <p className="about-us">
-      Welcome to Furniture Auction, your premier destination for discovering and bidding on high-quality furniture pieces. Our platform connects buyers with a diverse range of furniture from various sellers, offering a unique and competitive shopping experience. At Furniture Auction, we believe that finding the perfect piece of furniture should be exciting and accessible to everyone. Whether you are looking for modern, vintage, or custom-made furniture, our extensive listings have something to suit every taste and budget. Our mission is to provide a seamless and enjoyable auction experience. We ensure all listings are thoroughly vetted and described in detail to help you make informed bidding decisions. With our secure and user-friendly platform, you can bid with confidence from the comfort of your home. Thank you for choosing Furniture Auction. We are dedicated to bringing you the best in furniture auctions and look forward to helping you find your next treasured piece.
+    Welcome to our Thrift Store! your ultimate destination for discovering unique thrift items and placing bids to win your favorite finds. Our app connects thrifters with a wide array of pre-loved treasures, from vintage clothing and accessories to rare collectibles and home decor.
+    We believe in the value of secondhand items and the stories they carry. Join our community of thrifters today and start uncovering hidden gems!
     </p>
   </div>
 );
